0e0b4340f01cb9ab648538c11217e1619e2152ed
liluo <i@liluo.org>
2013-03-03 14:27:20 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fenbi::Application.config.secret_token = '97533c4f1eb8ab0fd193dac1c73b959a92180a3c30b41b82e624c70ff6c5369c4b0e7e8b3f25abba8aeafdff36d91f44a875dbb667f974e7973e284ca1838e31'
